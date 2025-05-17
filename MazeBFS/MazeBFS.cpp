28eb72ec46cc7fda95b34d86bf2a832335056dde
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-05-17 14:37:17 +0500

    implemented the basic BFS maze solving algo

--- MazeBFS/MazeBFS.cpp
+++ MazeBFS/MazeBFS.cpp
@@ -1,20 +1,144 @@
-// MazeBFS.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <algorithm>
+class Graph
+{
+    std::unordered_map<int, std::vector<int>> adjList;
+public:
+    void addEdge(int u, int v, bool bidirection = false)
+    {
+        adjList[u].push_back(v);
+
+        if (bidirection)
+            adjList[v].push_back(u);
+    }
+    void displayList() const
+    {
+        for (auto i : adjList)
+        {
+            std::cout << i.first << " -> ";
+            for (auto j : i.second)
+                std::cout << j << ", ";
+            std::cout << std::endl;
+        }
+    }
+    std::vector<std::pair<int, int>> bfs(int start, int end, int rows, int cols, std::unordered_map<int, bool> visited)
+    {
+        std::unordered_map<int, int> parent;
+
+        std::queue<int> q;
+        q.push(start);
+        visited[start] = true;
+        parent[start] = -1;
+
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+
+            if (node == end)
+                break;
+
+            for (auto neighbour : adjList[node])
+            {
+                if (!visited[neighbour])
+                {
+                    visited[neighbour] = true;
+                    parent[neighbour] = node;
+                    q.push(neighbour);
+                }
+            }
+        }
+
+        std::vector<int> path;
+        int current = end;
+        while (current != -1)
+        {
+            path.push_back(current);
+            current = parent[current];
+        }
+
+        reverse(path.begin(), path.end());
+
+        std::vector<std::pair<int, int>> pathCoords;
+
+        for (auto node : path)
+        {
+            int r = node / cols;
+            int c = node % cols;
+
+            pathCoords.push_back({ r, c });
+        }
+
+        return pathCoords;
+    }
+};
+
+void mazeToGraph(std::vector<std::vector<int>>& maze, Graph& g)
+{
+    int rows = maze.size();
+    int cols = maze[0].size();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+
+            if (maze[i][j])
+            {
+                std::vector<std::pair<int, int>> dirs = { {-1,0},{1,0},{0,-1},{0,1} };
+
+                int node = i * cols + j;
+
+                for (auto pair : dirs)
+                {
+                    int nr = i + pair.first;
+                    int nc = j + pair.second;
+
+                    if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && maze[nr][nc])
+                    {
+                        int neighbour = nr * cols + nc;
+                        g.addEdge(node, neighbour);
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::vector<std::vector<int>> maze = {
+    {1, 0, 1, 1, 1, 0, 1, 1, 1, 1},
+    {1, 0, 1, 0, 1, 1, 1, 0, 0, 1},
+    {1, 1, 1, 0, 0, 0, 1, 0, 1, 1},
+    {0, 0, 1, 1, 1, 0, 1, 0, 1, 0},
+    {1, 1, 0, 0, 1, 0, 1, 1, 1, 1},
+    {1, 0, 0, 1, 1, 1, 0, 0, 0, 1},
+    {1, 0, 1, 1, 0, 0, 0, 1, 0, 1},
+    {1, 1, 1, 0, 0, 1, 1, 1, 0, 1},
+    {0, 0, 1, 1, 1, 0, 0, 1, 1, 1},
+    {1, 1, 1, 0, 1, 1, 1, 0, 0, 1}
+    };
+
+    int rows = maze.size();
+    int cols = maze[0].size();
+
+    Graph g;
+
+    mazeToGraph(maze, g);
+
+    std::unordered_map<int, bool> visited;
+    std::vector<std::pair<int, int>> pathCoords;
+    pathCoords = g.bfs(0, rows * cols - 1, rows, cols, visited);
+
+    
+
+    
+
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
